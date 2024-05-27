823aa760affa5b4142d0df64f79adfe374f0a57c
Ali Altıparmak <ali@huridocs.org>
2024-05-27 17:28:42 +0300

    Add tests

--- src/test_end_to_end.py
+++ src/test_end_to_end.py
@@ -1,7 +1,5 @@
-from unittest import TestCase
-
 import requests
-
+from unittest import TestCase
 from configuration import ROOT_PATH
 
 
@@ -32,6 +30,63 @@ class TestEndToEnd(TestCase):
             results = requests.post(f"{self.service_url}", files=files)
 
             self.assertEqual(200, results.status_code)
+            self.assertEqual(2, len(results.json()))
+
+    def test_image_pdfs(self):
+        with open(f"{ROOT_PATH}/test_pdfs/image.pdf", "rb") as stream:
+            files = {"file": stream}
+
+            results = requests.post(f"{self.service_url}", files=files)
+
+            self.assertEqual(200, results.status_code)
+
+    def test_regular_pdf(self):
+        with open(f"{ROOT_PATH}/test_pdfs/regular.pdf", "rb") as stream:
+            files = {"file": stream}
+
+            results = requests.post(f"{self.service_url}", files=files)
+
+            self.assertEqual(200, results.status_code)
+
+    def test_error_file_fast(self):
+        with open(f"{ROOT_PATH}/test_pdfs/error.pdf", "rb") as stream:
+            files = {"file": stream}
+
+            results = requests.post(f"{self.service_url}/fast", files=files)
+
+            self.assertEqual(422, results.status_code)
+
+    def test_blank_pdf_fast(self):
+        with open(f"{ROOT_PATH}/test_pdfs/blank.pdf", "rb") as stream:
+            files = {"file": stream}
+
+            results = requests.post(f"{self.service_url}/fast", files=files)
+
+            self.assertEqual(200, results.status_code)
             self.assertEqual(0, len(results.json()))
 
+    def test_segmentation_some_empty_pages_fast(self):
+        with open(f"{ROOT_PATH}/test_pdfs/some_empty_pages.pdf", "rb") as stream:
+            files = {"file": stream}
 
+            results = requests.post(f"{self.service_url}/fast", files=files)
+
+            self.assertEqual(200, results.status_code)
+            self.assertEqual(2, len(results.json()))
+
+    def test_image_pdfs_fast(self):
+        with open(f"{ROOT_PATH}/test_pdfs/image.pdf", "rb") as stream:
+            files = {"file": stream}
+
+            results = requests.post(f"{self.service_url}/fast", files=files)
+
+            self.assertEqual(200, results.status_code)
+            self.assertEqual(0, len(results.json()))
+
+    def test_regular_pdf_fast(self):
+        with open(f"{ROOT_PATH}/test_pdfs/regular.pdf", "rb") as stream:
+            files = {"file": stream}
+
+            results = requests.post(f"{self.service_url}/fast", files=files)
+
+            self.assertEqual(200, results.status_code)
