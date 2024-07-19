1ebb872548b02d21d78199a62499f17ea7699273
Gabo <gabriel.piles.glez@gmail.com>
2024-07-19 14:37:21 +0200

    Async fast endpoint

--- src/pdf_layout_analysis/run_pdf_layout_analysis_fast.py
+++ src/pdf_layout_analysis/run_pdf_layout_analysis_fast.py
@@ -1,4 +1,6 @@
+import os
 from os.path import join
+from pathlib import Path
 from typing import AnyStr
 
 from fast_trainer.ParagraphExtractorTrainer import ParagraphExtractorTrainer
@@ -8,14 +10,20 @@ from pdf_layout_analysis.run_pdf_layout_analysis import pdf_content_to_pdf_path
 from pdf_tokens_type_trainer.TokenTypeTrainer import TokenTypeTrainer
 from pdf_tokens_type_trainer.ModelConfiguration import ModelConfiguration
 
-from configuration import ROOT_PATH, service_logger
+from configuration import ROOT_PATH, service_logger, XMLS_PATH
 from data_model.SegmentBox import SegmentBox
 
 
 def analyze_pdf_fast(file: AnyStr, xml_file_name: str = "") -> list[dict]:
     pdf_path = pdf_content_to_pdf_path(file)
     service_logger.info("Creating Paragraph Tokens [fast]")
-    pdf_features = PdfFeatures.from_pdf_path(pdf_path, xml_file_name)
+
+    xml_path = Path(join(XMLS_PATH, xml_file_name)) if xml_file_name else None
+
+    if xml_path and not xml_path.parent.exists():
+        os.makedirs(xml_path.parent, exist_ok=True)
+
+    pdf_features = PdfFeatures.from_pdf_path(pdf_path, str(xml_path) if xml_path else None)
     token_type_trainer = TokenTypeTrainer([pdf_features], ModelConfiguration())
     token_type_trainer.set_token_types(join(ROOT_PATH, "models", "token_type_lightgbm.model"))
     trainer = ParagraphExtractorTrainer(pdfs_features=[pdf_features], model_configuration=PARAGRAPH_EXTRACTION_CONFIGURATION)
