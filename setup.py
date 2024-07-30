07ea36a82d88fdf361ae7328d6deb3d65866a08a
Gabo <gabriel.piles.glez@gmail.com>
2024-07-30 12:27:50 +0200

    Change transformers version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name=PROJECT_NAME,
     packages=["pdf_tokens_type_trainer", "pdf_features", "pdf_token_type_labels", "fast_trainer"],
     package_dir={"": "src"},
-    version="0.4",
+    version="0.5",
     url="https://github.com/huridocs/pdf-document-layout-analysis",
     author="HURIDOCS",
     description="This tool is for PDF document layout analysis",
