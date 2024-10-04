192ae9daa58ab32d0a6ba439686aec91226e86f8
Gabo <gabriel.piles.glez@gmail.com>
2024-10-04 16:27:33 +0200

    Update version

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
+from pathlib import Path
+
 from setuptools import setup
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
+
+requirements_path = Path("requirements.txt")
+dependency_links = [r for r in requirements_path.read_text().splitlines() if r.startswith("git+")]
+requirements = [r for r in requirements_path.read_text().splitlines() if not r.startswith("git+")]
 
 PROJECT_NAME = "pdf-document-layout-analysis"
 
@@ -9,10 +13,11 @@ setup(
     name=PROJECT_NAME,
     packages=["pdf_tokens_type_trainer", "pdf_features", "pdf_token_type_labels", "fast_trainer"],
     package_dir={"": "src"},
-    version="0.9",
+    version="0.10",
     url="https://github.com/huridocs/pdf-document-layout-analysis",
     author="HURIDOCS",
     description="This tool is for PDF document layout analysis",
     install_requires=requirements,
-    setup_requieres=requirements,
+    setup_requires=requirements,
+    dependency_links=dependency_links,
 )
