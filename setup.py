67365bb133dab5826a3863d2e2cc551a21b89e81
Gabo <gabriel.piles.glez@gmail.com>
2024-10-07 12:06:02 +0200

    Downgrade huggingface hub library

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     name=PROJECT_NAME,
     packages=["pdf_tokens_type_trainer", "pdf_features", "pdf_token_type_labels", "fast_trainer"],
     package_dir={"": "src"},
-    version="0.14",
+    version="0.15",
     url="https://github.com/huridocs/pdf-document-layout-analysis",
     author="HURIDOCS",
     description="This tool is for PDF document layout analysis",
