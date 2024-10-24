0795230864cab07a1ae4a8ae901ca6c7ed8556f8
Ali Altıparmak <ali6parmakk@gmail.com>
2024-10-24 13:32:19 +0300

    Check Arabic text inside formula segments

--- src/extraction_formats/extract_formula_formats.py
+++ src/extraction_formats/extract_formula_formats.py
@@ -6,6 +6,10 @@ from fast_trainer.PdfSegment import PdfSegment
 from pdf_token_type_labels.TokenType import TokenType
 
 
+def has_arabic(text: str) -> bool:
+    return any("\u0600" <= char <= "\u06FF" or "\u0750" <= char <= "\u077F" for char in text)
+
+
 def get_latex_format(model: LatexOCR, formula_image: Image):
     buffer = io.BytesIO()
     formula_image.save(buffer, format="jpeg")
@@ -24,6 +28,8 @@ def extract_formula_format(pdf_images: PdfImages, predicted_segments: list[PdfSe
     model = LatexOCR()
 
     for index, formula_segment in formula_segments:
+        if has_arabic(formula_segment.text_content):
+            continue
         page_image: Image = pdf_images.pdf_images[formula_segment.page_number - 1]
         left, top = formula_segment.bounding_box.left, formula_segment.bounding_box.top
         width, height = formula_segment.bounding_box.width, formula_segment.bounding_box.height
