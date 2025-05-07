cc6b59d72d53dc1b9c011c2f72382cfce556bc8f
Ali Altıparmak <ali6parmakk@gmail.com>
2025-05-07 17:06:50 +0300

    Improve TitleType

--- src/pdf_features/TitleType.py
+++ src/pdf_features/TitleType.py
@@ -1,9 +1,7 @@
-from dataclasses import dataclass
 from enum import StrEnum
 from pdf_token_type_labels.TokenType import TokenType
 
 
-@dataclass
 class HeightRatioThresholds:
     H1 = 2.0
     H2 = 1.4
@@ -17,18 +15,18 @@ class TitleType(StrEnum):
     H4 = "H4"
     NO_TITLE = "No title"
 
-    @staticmethod
-    def from_height_ratio(height_ratio: float, token_type: TokenType) -> "TitleType":
+    @classmethod
+    def from_height_ratio(cls, height_ratio: float, token_type: TokenType) -> "TitleType":
         if token_type not in {TokenType.TITLE, TokenType.SECTION_HEADER}:
-            return TitleType.NO_TITLE
+            return cls.NO_TITLE
         if height_ratio > HeightRatioThresholds.H1:
-            return TitleType.H1
+            return cls.H1
         elif height_ratio > HeightRatioThresholds.H2:
-            return TitleType.H2
+            return cls.H2
         elif height_ratio > HeightRatioThresholds.H3:
-            return TitleType.H3
+            return cls.H3
         else:
-            return TitleType.H4
+            return cls.H4
 
     @property
     def markdown(self) -> str:
@@ -38,15 +36,13 @@ class TitleType(StrEnum):
     def html(self) -> str:
         return {TitleType.H1: "h1", TitleType.H2: "h2", TitleType.H3: "h3", TitleType.H4: "h4"}.get(self, "")
 
-    @classmethod
-    def get_styled_content_markdown(cls, content: str, title_type: "TitleType") -> str:
-        if title_type == cls.NO_TITLE:
+    def get_styled_content_markdown(self, content: str) -> str:
+        if self == TitleType.NO_TITLE:
             return content
-        return title_type.markdown + " " + content
+        return self.markdown + " " + content.strip()
 
-    @classmethod
-    def get_styled_content_html(cls, content: str, title_type: "TitleType") -> str:
-        if title_type == cls.NO_TITLE:
+    def get_styled_content_html(self, content: str) -> str:
+        if self == TitleType.NO_TITLE:
             return content
-        tag = title_type.html
-        return f"<{tag}>{content}</{tag}>"
+        tag = self.html
+        return f"<{tag}>{content.strip()}</{tag}>"
